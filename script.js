f701e375e7bb80709c059d4214e4c9328f8e902a
Opeyemil-code <opeyemitoheeb31@gmail.com>
2022-08-11 06:26:49 +0000

    first js project

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict'
+
+document.querySelector('body').textContent;
+document.querySelector('.prev').textContent;
+document.querySelector('.next').textContent;
+document.querySelector('.flex-3').textContent;
+document.querySelector('.closca').textContent;
+document.querySelector('.glacier').textContent;
+document.querySelector('.number').textContent;
+
+
+const prev= document.querySelector('.prev').textContent;
+if () {
+    
+}
+
+
+document.querySelector('.prev').addEventListener(
+    'click', function (params) {
+        document.querySelector('body').style.backgroundColor = '#F79256';
+        document.querySelector('body').style.color= 'black';
+        document.querySelector('.glacier').textContent = 'Beach';
+        document.querySelector('.flex-3').textContent ='2/4';
+        document.querySelector('.flex-3').style.fontSize= '20px';
+        
+    }
+)
+
